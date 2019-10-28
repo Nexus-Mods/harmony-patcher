555cb42382c917073a833f52bd84bb153b9598ab
IDCs <idc.dts@gmail.com>
2019-10-28 13:04:22 +0000

    Adding missing UI scripts

---
+++ VortexUnity/UIScripts/LogoFade.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace VortexUnity
+{
+    public class LogoFade : MonoBehaviour
+    {
+        private const float FADE_TIME = 1f;
+        private const float MIN_ALPHA = 0.5f;
+
+        private Color m_InitialColor;
+        public Color m_WantedColor;
+        private Image m_LogoRef = null;
+        private float m_fStartTime = 0f;
+
+        void Start()
+        {
+            m_fStartTime = Time.time;
+            m_LogoRef = GetComponent<Image>();
+            m_InitialColor = m_LogoRef.color;
+            m_WantedColor = new Color(m_InitialColor.r, m_InitialColor.g, m_InitialColor.b, MIN_ALPHA);
+            if (m_LogoRef != null)
+            {
+                StartCoroutine(FadeLoop());
+            }
+        }
+
+        private IEnumerator FadeLoop()
+        {
+            while (true)
+            {
+                float t = (Mathf.Sin(Time.time - m_fStartTime) * FADE_TIME);
+                m_LogoRef.color = Color.Lerp(m_InitialColor, m_WantedColor, t);
+
+                yield return null;
+            }
+        }
+    }
+}
---
+++ VortexUnity/UIScripts/ScrollScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace VortexUnity
+{
+    public class ScrollScript : MonoBehaviour
+    {
+        public float m_fScrollSpeed = -5f;
+        private float m_rectWidth = 20f;
+        private Vector2 m_v2InitialPos = Vector2.zero;
+
+        private RectTransform[] m_children = null;
+
+        void Start()
+        {
+            m_v2InitialPos = new Vector2(0f, Screen.height * 0.5f);
+            transform.position = m_v2InitialPos;
+            Rect rect = GetComponent<RectTransform>().rect;
+            m_rectWidth = rect.width;
+
+            m_children = GetComponentsInChildren<RectTransform>()
+                .Where(child => child.transform != transform)
+                .ToArray();
+
+            if (m_children.Length >= 2)
+            {
+                m_children[0].transform.localPosition = new Vector2(m_rectWidth, 0f);
+                m_children[1].transform.localPosition = new Vector2(-m_rectWidth, 0f);
+            }
+        }
+
+        void Update()
+        {
+            float fNewPos = Mathf.Repeat(Time.time * m_fScrollSpeed, m_rectWidth);
+            transform.position = m_v2InitialPos + (Vector2.right * fNewPos);
+        }
+    }
+}
