26d031dd43d2f9e8b5d119bd0fb80694075996eb
IDCs <idc.dts@gmail.com>
2019-10-28 12:16:30 +0000

    Logger delegate fix

--- VortexHarmonyInstaller/Delegates/LoggerDelegates.cs
+++ VortexHarmonyInstaller/Delegates/LoggerDelegates.cs
@@ -9,12 +9,12 @@ namespace VortexHarmonyInstaller.Delegates
     public static class LoggerDelegates
     {
         public delegate void OnInfo(object something);
-        public static OnInfo LogInfo => VortexHarmonyInstaller.VortexPatcher.Logger.Info;
+        public static OnInfo LogInfo = (object something) => VortexHarmonyInstaller.VortexPatcher.Logger.Info(something);
 
         public delegate void OnError(object something, Exception e = null);
-        public static OnError LogError => VortexHarmonyInstaller.VortexPatcher.Logger.Error;
+        public static OnError LogError = (object data, Exception e) => VortexHarmonyInstaller.VortexPatcher.Logger.Error(data, e);
 
-        public delegate void OnDebug(object something);
-        public static OnDebug LogDebug => VortexHarmonyInstaller.VortexPatcher.Logger.Error;
+        public delegate void OnDebug(object data);
+        public static OnDebug LogDebug = (object data) => VortexHarmonyInstaller.VortexPatcher.Logger.Error(data);
     }
 }
