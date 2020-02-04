bde302ace80ecc5acb68301e310a2f951b189c57
IDCs <idc.dts@gmail.com>
2020-02-04 13:28:25 +0000

    fix: missing consent dialog export

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const patcher_1 = require('./patcher');
 exports.runPatcher = patcher_1.runPatcher;
 exports.initHarmonyUI = patcher_1.initHarmonyUI;
+exports.raiseConsentNotification = patcher_1.raiseConsentNotification;
